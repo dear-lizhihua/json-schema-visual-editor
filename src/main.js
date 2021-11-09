0261ef12a38c69dbaed4c1d1dcee85d83b6bb106
lizhihua <275091674@qq.com>
2021-11-09 18:04:41 +0800

    feat: update

--- src/main.js
+++ src/main.js
@@ -1,11 +1,29 @@
 import { createApp } from 'vue'
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
+
+// import Antd from 'ant-design-vue'
 import Example from './packages/example.vue'
+const app = createApp(Example)
 
 // import { FilterFilled } from '@ant-design/icons-vue'
 // app.component(FilterFilled.displayName, FilterFilled)
 
-const app = createApp(Example)
-app.use(Antd)
+import {
+  Input,
+  Row,
+  Tooltip,
+  Col,
+  Form,
+  Select,
+  Checkbox,
+  Button,
+  Modal,
+  message,
+  Tabs,
+  AutoComplete,
+} from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
+const replaceAntdComponentName = value => value.replace(/^A/g, '')
+app.component(replaceAntdComponentName(Button.name), Button)
+// app.use(Antd)
 app.mount('#app')
--- src/packages/json-schema-visual-editor/index.vue
+++ src/packages/json-schema-visual-editor/index.vue
@@ -1,14 +1,23 @@
 <template>
-  123
+  <Button class="import-json-button" type="primary" @click="showModal">
+    {{ localProvider('import_json') }}
+  </Button>
 </template>
 
 <script>
-import { defineComponent, inject } from 'vue'
+import { defineComponent, provide, inject } from 'vue'
+import localProvider from '/@/packages/components/local-provider'
 
 export default defineComponent({
   setup() {
+    provide('localProvider', localProvider)
     const store = inject('store')
     console.log(store)
+    const showModal = () => console.log('showModal')
+    return {
+      showModal,
+      localProvider,
+    }
   }
 })
 </script>
