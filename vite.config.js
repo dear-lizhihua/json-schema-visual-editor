412811dcaca47470bb5d0d33e55f9e809bd807e4
lizhihua <275091674@qq.com>
2021-11-09 16:25:03 +0800

    feat: update

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.js'),
+      fileName: 'index',
+      formats: ['esm'],
+    },
+    minify: false,
+    rollupOptions: {
+      external: [
+        '@ant-design-vue/use',
+        '@ant-design/icons-vue',
+        'ant-design-vue',
+        'ant-design-vue/dist/antd.css',
+        'vue',
+        'vue-router',
+      ],
+    }
+  },
+})
