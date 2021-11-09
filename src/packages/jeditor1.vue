108df5a9adc6a6a0a005fba40808f4f220f1d6c7
lizhihua <275091674@qq.com>
2021-11-09 17:46:23 +0800

    feat: update

--- src/packages/jeditor1.vue
+++ src/packages/jeditor1.vue
@@ -1,16 +1,31 @@
 <script>
-import {defineComponent} from 'vue'
-import {
-  JsonSchemaVisualEditor
-} from './json-schema-visual-editor'
+import { defineComponent, reactive, provide } from 'vue'
+import { JsonSchemaVisualEditor } from './json-schema-visual-editor'
+
+import schema from './models/schema'
 
 export default defineComponent({
+  props: {
+    mock: Array,
+    showEditor: Boolean,
+    data: String,
+    onChange: Function
+  },
   components: {JsonSchemaVisualEditor},
+  setup(props) {
+    const store = reactive({schema})
+    store.__jsonSchemaMock = props.mock
+
+    provide('store', store)
+
+    return {}
+  }
 })
 </script>
 
 <template>
-  <JsonSchemaVisualEditor />
+  <JsonSchemaVisualEditor
+  />
 </template>
 
 <style lang="scss" scoped>
--- src/packages/json-schema-visual-editor/index.vue
+++ src/packages/json-schema-visual-editor/index.vue
@@ -1,3 +1,14 @@
 <template>
   123
 </template>
+
+<script>
+import { defineComponent, inject } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const store = inject('store')
+    console.log(store)
+  }
+})
+</script>
